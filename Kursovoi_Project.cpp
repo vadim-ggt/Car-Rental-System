2f96153feea6a09ab0d4d1d2da88f6d2351c00a0
Vaddim <135662809+vadim-ggt@users.noreply.github.com>
2025-02-25 21:27:29 +0300

    Add files via upload
    это мэйн

---
+++ Kursovoi_Project.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+#include "CLASS_CAR_RENTAL_SYSTEM.h"
+#include <windows.h>
+
+
+void setBackgroundColor(int backgroundColor) {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	// Установить цвет текста (7 - стандартный белый) + цвет фона
+	SetConsoleTextAttribute(hConsole, 7 | (backgroundColor << 4));
+}
+
+
+
+
+// вход
+
+int main() {
+	setBackgroundColor(1);
+	CarRentalSystem rer;
+	rer.showMainMenu();
+	return 0;
+}
+
